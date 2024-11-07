78a8e842a4f0ae6ea4160087bfbbae026f80d1e2
Larisa Botha <larisabotha@gmail.com>
2024-11-07 22:18:59 +0200

    setup api endpoint url environment variable

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+/// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+    readonly VITE_API_URL: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
