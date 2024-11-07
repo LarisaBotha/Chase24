f1f22e5b2b078d31a6716b60513aba7fb5a817fe
Larisa Botha <larisabotha@gmail.com>
2024-11-07 12:47:49 +0200

    astro add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   server: {
     port: 8000,  // Set to your preferred port number
   },
-  
-  output: 'hybrid',
 
+  output: 'hybrid',
   integrations: [tailwind()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
